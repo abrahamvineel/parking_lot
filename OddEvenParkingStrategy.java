d8068bbe6583878a9fed89474afa388db2c083d9
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-10 21:19:45 -0400

    odd even parking strategy wip

---
+++ OddEvenParkingStrategy.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class OddEvenParkingStrategy implements ParkingStrategy {
+    @Override
+    public int getSpotNumber(List<ParkingSpot> parkingSpots) {
+        return 0;
+    }
+}
