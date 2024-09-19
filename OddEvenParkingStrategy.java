49145602a0859c6ff87bcdcd3150f05c43798bc9
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-09-18 20:56:15 -0400

    odd or even strategy

--- OddEvenParkingStrategy.java
+++ OddEvenParkingStrategy.java
@@ -1,8 +1,18 @@
 import java.util.List;
 
 public class OddEvenParkingStrategy implements ParkingStrategy {
+    private static boolean isEven = true;
     @Override
     public int getSpotNumber(List<ParkingSpot> parkingSpots) {
-        return 0;
+        for (int i = 0; i < parkingSpots.size(); i++) {
+            if(isEven && parkingSpots.get(i).isSpotAvailable()) {
+                isEven = false;
+                return i;
+            } else if (!isEven && parkingSpots.get(i).isSpotAvailable()) {
+                isEven = true;
+                return i;
+            }
+        }
+        return -1;
     }
 }
